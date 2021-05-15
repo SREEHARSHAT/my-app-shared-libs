24359a7da9f13c5a294e5178a13ddb601edf8e12
SREEHARSHAT <sreeharshatadepalli@gmail.com>
2021-05-15 23:36:45 +0530

    Update TomcatDeploy.groovy

--- vars/TomcatDeploy.groovy
+++ vars/TomcatDeploy.groovy
@@ -1,4 +1,4 @@
-def call(cred_id,user.ip){
+def call(cred_id,user,ip){
   sshagent([cred_id]) {
      sh "mv target/*war target/myweb.war"
      sh "scp -o StrictHostKeyChecking=no target/myweb.war ${user}@${ip}:/opt/tomcat8/webapps"
